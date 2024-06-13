0df16cb0112354e8c8a9451191149ca4194d2f20
Shehzad Khan <sh44z@live.co.uk>
2024-06-13 18:34:34 +0100

    added the pictures and description to the places page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <div className="bg-custom-cyan text-white py-4 flex justify-center items-center">
-      © The Light Blue House Ltd 2024
+      The Light Blue House Ltd 2024
     </div>
   );
 };
