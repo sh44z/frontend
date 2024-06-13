8b0ddce136f1514ae718ca5d95b2550dbe4cb747
Shehzad Khan <sh44z@live.co.uk>
2024-06-13 18:40:03 +0100

    added the pictures and description to the places page

--- src/app/(propertiesPage)/slonesqflat/page.js
+++ src/app/(propertiesPage)/slonesqflat/page.js
@@ -9,7 +9,7 @@ const page = () => {
   return (
     <div className="mt-4 -mx-8 px-8 pt-8">
         <h1 className='text-2xl'>Beautiful Sloane Square Apartment</h1>
-        <a className='my-2 block font-semibold underline' target='_blank' href={"https://www.google.com/maps/place/Sloane+Square,+London/@51.4927684,-0.1570174,17z/data=!3m1!4b1!4m6!3m5!1s0x4876051614d622e1:0x7918c355ffd357f5!8m2!3d51.4927684!4d-0.1570174!16zL20vMDJmODk2?entry=ttu"}>Sloane Square, London, UK</a><br/>
+        <a className='my-2 block font-semibold underline' target='_blank' href={"https://www.google.com/maps/place/Sloane+Square,+London/@51.4927684,-0.1570174,17z/data=!3m1!4b1!4m6!3m5!1s0x4876051614d622e1:0x7918c355ffd357f5!8m2!3d51.4927684!4d-0.1570174!16zL20vMDJmODJmODk2?entry=ttu"}>Sloane Square, London, UK</a><br/>
         <div className='grid gap-2 grid-cols-3 grid-rows-2 mb-4'>
             <div className="col-span-2 row-span-2">
                 <Image 
@@ -38,14 +38,14 @@ const page = () => {
                     className="w-full h-full object-cover"
                 />
             </div>
-    </div>
+        </div>
         <div className='mb-5'>
           <h2 className='font-bold text-2xl mb-2'>Description</h2>
           <p className='text-l'>
             About this space
               In the heart of Constable Country, on the Essex Way, nestled amongst farmers fields and woodland you will find The Willows.
 
-              A perfect balance of rustic and contemporary, The Willows provides a rurual get away with all the modern comforts. WIFI, Smart TV's, air con, jacuzzi bath, private outdoor hot tub and plenty of parking.
+              A perfect balance of rustic and contemporary, The Willows provides a rural get away with all the modern comforts. WIFI, Smart TV's, air con, jacuzzi bath, private outdoor hot tub and plenty of parking.
               It is positioned 5 minutes from an A12 link, 10 minutes from Colchester town and less than an hour from London.
           </p>
         </div>
